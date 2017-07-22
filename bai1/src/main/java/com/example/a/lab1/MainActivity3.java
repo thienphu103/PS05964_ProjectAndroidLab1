c593c68f020d26c269707dc61c0f40c9a3457adb
thienphu103 <thienphu103@gmail.com>
2017-07-22 11:05:08 +0700

    change

--- bai1/src/main/java/com/example/a/lab1/MainActivity3.java
+++ bai1/src/main/java/com/example/a/lab1/MainActivity3.java
@@ -15,6 +15,7 @@ public class MainActivity3 extends AppCompatActivity {
     int he=170;
     int wi=215;
     int z = 0;
+    int r = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
